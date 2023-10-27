d1955cadb792963368a85020db200449de5aa9c1
ansoo <ansoo97@naver.com>
2023-10-27 14:59:54 +0900

    10-27 Navbar

--- src/apis/mypage.js
+++ src/apis/mypage.js
@@ -1,4 +1,4 @@
-import axios from 'axios';
+
 import { getAuthAxios } from "./authAxios";
 
 
---
+++ src/pages/Search.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Search() {
+  return (
+    <div>
+      검색 페이지
+    </div>
+  )
+}

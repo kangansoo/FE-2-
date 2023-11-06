7ec7f6c19b5d31a7a3e798fb174bace34a184605
ansoo <ansoo97@naver.com>
2023-11-06 16:11:33 +0900

    1106()

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -10,7 +10,7 @@ export default function Main() {
 
   const responsive = {
     desktop: {
-      breakpoint: { max: 3000, min: 1024 },
+      breakpoint: { max: 5000, min: 0 },
       items: 5,
       slidesToSlide: 1 // optional, default to 1.
     }

a9e44df0dda2b748e9777a82d3bcc0d3963550d7
ansoo <ansoo97@naver.com>
2023-11-17 15:47:34 +0900

    :memo: Comment변수명 변경
    comment -> review로 전부 변경 json_server도 review로 변경 필

--- src/components/ReviewModal.js
+++ src/components/ReviewModal.js
@@ -8,8 +8,8 @@ export default function ReviewModal() {
     let {content_id}=useParams();
     const subsr= localStorage.getItem('subsr');
 
-    //comment get 요청 useState
-    const [comment, setComment] = useState();
+    //review get 요청 useState
+    const [review, setReview] = useState();
     //rating get 요청 useState
     const [rating, setRating] = useState();
 
@@ -31,21 +31,21 @@ export default function ReviewModal() {
       checkRatings();
     }, [subsr, content_id]);
 
-    //comment get요청
+    //review get요청
     useEffect(() => {
-      const checkComments = async () => {
+      const checkReviews = async () => {
         try {
-          const response = await axios.get('http://localhost:30/comments');
+          const response = await axios.get('http://localhost:30/reviews');
           const found = response.data.filter((item) => item.subsr === subsr && item.content_id === content_id);
           if (found.length > 0) {
-            setComment(found[found.length-1].comment);
+            setReview(found[found.length-1].review);
           } else{
           }
         } catch (error) {
           console.log(error);
         }
       };
-      checkComments();
+      checkReviews();
     }, [subsr, content_id]);
 
   let subtitle;
@@ -65,7 +65,7 @@ export default function ReviewModal() {
   }
 
   const handleChange = ((e)=> {
-    setComment(e.target.value);
+    setReview(e.target.value);
   })
 
     const rate = (rating)=>{
@@ -77,14 +77,12 @@ export default function ReviewModal() {
         await axios.post("http://localhost:30/ratings", rating_info);
     };
 
-    const handleComment = async() => {
-      if (comment.length === 0) {
-        setComment(null);
-        const comment_info={subsr:subsr, content_id:content_id, comment:comment};
-        await axios.post("http://localhost:30/comments", comment_info);
+    const handleReview = async() => {
+      if (review.length === 0) {
+        setReview();
       }else{
-        const comment_info={subsr:subsr, content_id:content_id, comment:comment};
-        await axios.post("http://localhost:30/comments", comment_info);
+        const review_info={subsr:subsr, content_id:content_id, review:review};
+        await axios.post("http://localhost:30/reviews", review_info);
       }
     }
 
@@ -94,7 +92,7 @@ export default function ReviewModal() {
         e.preventDefault();
       }else{
         handleRating();
-        handleComment();
+        handleReview();
       }
     }
 
@@ -113,6 +111,7 @@ export default function ReviewModal() {
         <button onClick={closeModal}>×</button>
         <div>
         {<Rating
+            fillColor="#A50034"
             size="35"
             initialValue={rating}
             onClick={rate}
@@ -121,7 +120,7 @@ export default function ReviewModal() {
         <form>
           <textarea
           maxLength={100}
-          value={comment}
+          value={review}
           onChange={handleChange}
           placeholder='리뷰는 최대 100자까지 작성 가능합니다.'
           style={{width: '490px', height:'150px', border: '1px solid', tesxtAlign:'justify'}}

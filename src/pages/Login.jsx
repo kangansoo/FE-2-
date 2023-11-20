502b901027f9fa235458cd842011441cda6df6f3
eunhabaek <beh2549@gmail.com>
2023-11-20 10:19:47 +0900

    :safety_vest: 메인페이지 GET 요청 수정 (model 3개 한 번에 받아오기)
    - 이후 새로고침 기능 추가 예정 - json_server에 아래 내용 추가 필요함

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -69,7 +69,6 @@ const Login = () => {
         //navigate("/mypage")
     //};
 
-
   return (
     <Wrapper>
         <Title>로그인하기</Title>
@@ -78,7 +77,7 @@ const Login = () => {
                 <Input placeholder="셋탑박스" value={subsr} onChange={onChangeId}/>
                 {/*<Input placeholder="비밀번호" type="password" value={password} onChange={onChangePw}/>*/}
             </Inputs>
-            <Button onClick={onClick} onKeyDownCapture={navigate("/mypage")} disabled={button}>Login</Button>
+            <Button onClick={onClick}  disabled={button}>Login</Button>
         </Form>
         {/* 주석 처리 <CustomLink to='/signup'>회원가입하기</CustomLink> */}
     </Wrapper>

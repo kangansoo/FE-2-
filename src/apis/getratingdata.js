deae13a79eb74579f03a41815d832d30c5321139
kangansoo <ansoo971@gmail.com>
2023-11-21 13:41:22 +0900

    :zap: Api 명세서대로 api 수정

---
+++ src/apis/getratingdata.js
@@ -0,0 +1,6 @@
+import axios from "axios";
+
+export const getratingdata = async(content_id) => {
+    const response = await axios.get(`http://localhost:30/detail${content_id}rating`)
+    return response.data;
+}

14a395df4ad4d579a9b099a8396ed87b154f002f
eunhabaek <beh2549@gmail.com>
2023-12-06 11:10:16 +0900

    :alien: API cloudfront로 수정

--- src/apis/detail/getVodData.js
+++ src/apis/detail/getVodData.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const getVodData = async (content_id)=>{
-    const result = await axios.get(`http://lv3-loadbalancer-918926550.ap-northeast-2.elb.amazonaws.com/detail/${content_id}`) //(`http://localhost:30/detail${content_id}`)
+    const result = await axios.get(`https://d225nwg9l5o274.cloudfront.net/detail/${content_id}`) //(`http://localhost:30/detail${content_id}`)
     return result;
 }
 

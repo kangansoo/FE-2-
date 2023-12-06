d8e6ade06d0e8e5fc97b818a217bc5091afb67de
eunhabaek <beh2549@gmail.com>
2023-12-07 06:46:05 +0900

    https 테스트

--- src/apis/main/getmood.js
+++ src/apis/main/getmood.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 //mood VOD 리스트 가져오기
 export const moodList = async (mood) => {
-    const result = await axios.get(`http://lv3-loadbalancer-918926550.ap-northeast-2.elb.amazonaws.com/main/${mood}`) //(`http://localhost:30/main${mood}`)
+    const result = await axios.get(`d225nwg9l5o274.cloudfront.net/main/${mood}`) //(`http://localhost:30/main${mood}`)
     return result;
 };

5e3ca1aa50ec0e319b7c330b51c73fa2ce590014
eunhabaek <beh2549@gmail.com>
2023-12-06 11:27:35 +0900

    :alien: API http로 수정

--- src/apis/mypage/getmypagewish_post.js
+++ src/apis/mypage/getmypagewish_post.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export const getmypagewish = async (subsr) => {
-    const result = await axios.post('https://d225nwg9l5o274.cloudfront.net/mypage/wish', {subsr}) //('http://localhost:30/mypagewish', {subsr})
+    const result = await axios.post('http://lv3-loadbalancer-918926550.ap-northeast-2.elb.amazonaws.com/mypage/wish', {subsr}) //('http://localhost:30/mypagewish', {subsr})
     return result;
 }
+

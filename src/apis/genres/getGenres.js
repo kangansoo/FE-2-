da21c54f3d0cb12f57f3e29a0448f536496bed60
eunhabaek <beh2549@gmail.com>
2023-12-07 09:04:26 +0900

    :alien: Api HTTP로 수정

--- src/apis/genres/getGenres.js
+++ src/apis/genres/getGenres.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 //전체 VOD 카테고리 가져오기
 export const genres = async () => {
-    const result = await axios.get('https://d225nwg9l5o274.cloudfront.net/genres'); //("http://localhost:30/genres");
+    const result = await axios.get('http://lv3-loadbalancer-918926550.ap-northeast-2.elb.amazonaws.com/genres'); //("http://localhost:30/genres");
     return result;
 };
